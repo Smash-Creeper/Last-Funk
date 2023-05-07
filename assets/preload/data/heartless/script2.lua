cfeac98d6748df13404271db45042d27b2dd42fd
JustyyDev <110690806+JustyyDev@users.noreply.github.com>
2023-05-07 22:43:06 +0200

    lol i added and edited more stuff very funny

--- assets/preload/data/heartless/script2.lua
+++
@@ -1,6 +0,0 @@
-function onUpdate()
-	if botPlay then
-		loadSong('totally-normal-song', 'totally-normal-song', true);
-		debugPrint('works');
-	end
-end
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1110,8 +1110,8 @@ class PlayState extends MusicBeatState
 		add(healthBarBG);
 		if(ClientPrefs.downScroll) healthBarBG.y = 0.11 * FlxG.height;
 
-		healthBar = new FlxBar(healthBarBG.x + 4, healthBarBG.y + 4, RIGHT_TO_LEFT, Std.int(healthBarBG.width - 8), Std.int(healthBarBG.height - 8), this,
-			'health', 0, 2);
+		healthBar = new FlxBar(healthBarBG.x + 8, healthBarBG.y + 32, RIGHT_TO_LEFT, Std.int(healthBarBG.width - 28), Std.int(healthBarBG.height - 38), this,
+		 'health', 0, 2);
 		healthBar.scrollFactor.set();
 		// healthBar
 		healthBar.visible = !ClientPrefs.hideHud;
